c78c0c698c157d041bfce8c119691bef3f635a75
Jairo Llopis <yajo.sk8@gmail.com>
2022-08-24 10:24:40 +0100

    test: assert systemd-unit installation
    This test demonstrates that the recipe for partitioning found in the README didn't really work.
    
    @moduon MT-1075

--- tests/test.nix
+++ tests/test.nix
@@ -8,13 +8,36 @@ let
       inherit (pkgs) system;
     };
   disko-config = import ../example/raid.nix;
+  wrapScript = pkgs: script:
+    let
+      diskoEnv = pkgs.symlinkJoin {
+        name = "diskoEnv";
+        paths = with pkgs;[
+          # Device and partition tools
+          cryptsetup
+          lvm2.bin
+          mdadm
+          parted
+
+          # Packages that provide a mkfs.* binary
+          btrfs-progs
+          dosfstools
+          e2fsprogs
+          f2fs-tools
+          nilfs-utils
+          util-linux
+          xfsprogs
+        ];
+      } ;
+    in pkgs.writeScript "wrapped" ''
+      #!${pkgs.bash}/bin/bash
+      export PATH=${diskoEnv}/bin:$PATH
+      ${script}
+    '';
   tsp-create = pkgs.writeScript "create" ((pkgs.callPackage ../. {}).create disko-config);
   tsp-mount = pkgs.writeScript "mount" ((pkgs.callPackage ../. {}).mount disko-config);
-in makeTest' {
-  name = "disko";
-
-  nodes.machine =
-    { config, pkgs, modulesPath, ... }:
+  baseConfig =
+    { modulesPath, ... }:
 
     {
       imports = [
@@ -25,14 +48,62 @@ in makeTest' {
       # speed-up eval
       documentation.enable = false;
 
-      virtualisation.emptyDiskImages = [ 512 512 ];
+      virtualisation.diskSize = 512;
+      virtualisation.emptyDiskImages = [ 32 32 ];
     };
+in makeTest' {
+  name = "disko";
+
+  nodes.manual1 = baseConfig;
+
+  nodes.automatic1 = {pkgs, modulesPath, ...}: {
+    imports = [
+      baseConfig
+      (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+    ];
+    systemd.services.install-to-hd = {
+      enable = true;
+      wantedBy = ["multi-user.target"];
+      after = ["getty@tty1.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = [
+          (wrapScript pkgs tsp-create)
+          (wrapScript pkgs tsp-mount)
+        ];
+        StandardInput = "null";
+        StandardOutput = "journal+console";
+        StandardError = "inherit";
+      };
+    };
+  };
 
   testScript = ''
-    machine.succeed("echo 'secret' > /tmp/secret.key");
-    machine.succeed("${tsp-create}");
-    machine.succeed("${tsp-mount}");
-    machine.succeed("${tsp-mount}"); # verify that the command is idempotent
-    machine.succeed("test -b /dev/md/raid1");
+    EXPECTED_BLKS = """
+    vda 253:0 0 512M 0 disk /
+    vdb 253:16 0 32M 0 disk
+    vdb1 253:17 0 31M 0 part
+    md127 9:127 0 29.9M 0 raid1
+    md127p1 259:0 0 28.9M 0 part
+    vdc 253:32 0 32M 0 disk
+    vdc1 253:33 0 31M 0 part
+    md127 9:127 0 29.9M 0 raid1
+    md127p1 259:0 0 28.9M 0 part
+    """
+
+    start_all()
+
+    manual1.succeed("${tsp-create}");
+    manual1.succeed("${tsp-mount}");
+    manual1.succeed("${tsp-mount}"); # verify that the command is idempotent
+
+    for node in [manual1, automatic1]:
+      node.wait_for_file("/mnt/raid")
+      node.succeed("test -b /dev/md/raid1")
+
+      blks = node.succeed("lsblk --raw")
+      print(blks)
+      for line in EXPECTED_BLKS.strip().splitlines():
+        assert line in blks, f"Line not found: {line}"
   '';
 }
