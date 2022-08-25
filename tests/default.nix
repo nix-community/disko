1ac0e76b15a3b13c83d25a78b32dee4070a584b7
lassulus <lassulus@lassul.us>
2022-08-25 23:12:49 +0200

    tests: add lib, autoimport tests

--- tests/default.nix
+++ tests/default.nix
@@ -1,8 +1,15 @@
 { makeTest ? import <nixpkgs/nixos/tests/make-test-python.nix>
 , pkgs ? (import <nixpkgs> {})
 }@args:
-{
-  luks-lvm = import ./luks-lvm.nix args;
-  mdadm = import ./mdadm.nix args;
-  zfs = import ./zfs.nix args;
-}
+let
+  lib = pkgs.lib;
+  makeDiskoTest = (pkgs.callPackage ./lib.nix { inherit makeTest; }).makeDiskoTest;
+  allTestFilenames =
+    builtins.map (lib.removeSuffix ".nix") (
+      builtins.filter
+        (x: lib.hasSuffix ".nix" x && x != "default.nix" && x != "lib.nix")
+        (lib.attrNames (builtins.readDir ./.))
+    );
+
+  allTests = lib.genAttrs (allTestFilenames) (test: import (./. + "/${test}.nix") { inherit makeDiskoTest; });
+in allTests
--- tests/luks-lvm.nix
+++ tests/luks-lvm.nix
@@ -1,38 +1,9 @@
-{ makeTest ? import <nixpkgs/nixos/tests/make-test-python.nix>
-, pkgs ? (import <nixpkgs> {})
+{ pkgs ? (import <nixpkgs> {})
+, makeDiskoTest ? (pkgs.callPackage ./lib.nix {}).makeDiskoTest
 }:
-let
-  makeTest' = args:
-    makeTest args {
-      inherit pkgs;
-      inherit (pkgs) system;
-    };
+makeDiskoTest {
   disko-config = import ../example/luks-lvm.nix;
-  tsp-create = pkgs.writeScript "create" ((pkgs.callPackage ../. {}).create disko-config);
-  tsp-mount = pkgs.writeScript "mount" ((pkgs.callPackage ../. {}).mount disko-config);
-in makeTest' {
-  name = "disko";
-
-  nodes.machine =
-    { config, pkgs, modulesPath, ... }:
-
-    {
-      imports = [
-        (modulesPath + "/profiles/installation-device.nix")
-        (modulesPath + "/profiles/base.nix")
-      ];
-
-      # speed-up eval
-      documentation.enable = false;
-
-      virtualisation.emptyDiskImages = [ 512 ];
-    };
-
-  testScript = ''
-    machine.succeed("echo 'secret' > /tmp/secret.key");
-    machine.succeed("${tsp-create}");
-    machine.succeed("${tsp-mount}");
-    machine.succeed("${tsp-mount}"); # verify that the command is idempotent
+  extraTestScript = ''
     machine.succeed("cryptsetup isLuks /dev/vdb2");
     machine.succeed("grep -qs '/mnt/home' /proc/mounts");
   '';
