380fcb30268e877ba15b577b0a6bbd2a2cc7f96e
Jörg Thalheim <joerg@thalheim.io>
2022-09-05 14:11:32 +0200

    fix example/default.nix

--- example/default.nix
+++ example/default.nix
@@ -1,12 +1,11 @@
 # usage: nix-instantiate --eval --json --strict example | jq -r .
 
 let
-  # TODO: get rid of NIX_PATH dependency here
-  pkgs = import <nixpkgs> {};
   cfg = import ./config.nix;
   #cfg = import ./config-gpt-bios.nix;
 in
-with import ../lib { inherit (pkgs) lib;};
+# TODO: get rid of NIX_PATH dependency here
+with import ../. {};
 
 {
   config = config cfg;
