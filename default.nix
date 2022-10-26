324a8d5347a18e0c74625903c3bb13144370fbf2
lassulus <lassulus@lassul.us>
2022-10-27 00:02:37 +0200

    disko: add scripts with pkgs

--- default.nix
+++ default.nix
@@ -15,7 +15,15 @@ let
 in {
   types = types;
   create = cfg: types.diskoLib.create (eval cfg).config.devices;
+  createScript = pkgs: cfg: pkgs.writeScript "disko-create" ''
+    export PATH=${lib.makeBinPath (types.diskoLib.packages (eval cfg).config.devices pkgs)}
+    ${types.diskoLib.create (eval cfg).config.devices}
+  '';
   mount = cfg: types.diskoLib.mount (eval cfg).config.devices;
+  mountScript = pkgs: cfg: pkgs.writeScript "disko-mount" ''
+    export PATH=${lib.makeBinPath (types.diskoLib.packages (eval cfg).config.devices pkgs)}
+    ${types.diskoLib.mount (eval cfg).config.devices}
+  '';
   config = cfg: { imports = types.diskoLib.config (eval cfg).config.devices; };
   packages = cfg: types.diskoLib.packages (eval cfg).config.devices;
 }
