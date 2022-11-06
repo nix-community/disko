cf2433c6fb0fe10ed1f4ab7d13933a0e5b9b8b3f
David Arnold <david.arnold@iohk.io>
2022-11-05 22:29:28 -0500

    fix: expect normal disko-config module files

--- cli.nix
+++ cli.nix
@@ -3,8 +3,8 @@
 , diskoFile
 , ... }@args:
 let
-  disko = import ./. {};
-  diskFormat = import diskoFile args;
+  disko = import ./. { inherit (pkgs) lib; };
+  diskFormat = import diskoFile;
   diskoEval = if (mode == "create") then
     disko.createScript diskFormat pkgs
   else if (mode == "mount") then
