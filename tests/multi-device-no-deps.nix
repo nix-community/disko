d61fd1522b0477b376418edb9dc1ea336af29f89
lassulus <lassulus@lassul.us>
2022-11-10 09:46:55 +0100

    add regression test for #52

---
+++ tests/multi-device-no-deps.nix
@@ -0,0 +1,12 @@
+# this is a regression test for https://github.com/nix-community/disko/issues/52
+{ pkgs ? (import <nixpkgs> { })
+, makeDiskoTest ? (pkgs.callPackage ./lib.nix { }).makeDiskoTest
+}:
+makeDiskoTest {
+  disko-config = ../example/multi-device-no-deps.nix;
+  testBoot = false;
+  extraTestScript = ''
+    machine.succeed("mountpoint /mnt/a");
+    machine.succeed("mountpoint /mnt/b");
+  '';
+}
