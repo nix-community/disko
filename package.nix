0fadf137556d6578ebf60bbc7d402729d8f1d41c
Jörg Thalheim <joerg@thalheim.io>
2023-02-02 13:58:43 +0100

    hardcode nixpkgs in NIX_PATH of disko cli

--- package.nix
+++ package.nix
@@ -1,8 +1,11 @@
-{ stdenvNoCC, lib }:
+{ stdenvNoCC, makeWrapper, lib, path }:
 
 stdenvNoCC.mkDerivation rec {
   name = "disko";
   src = ./.;
+  nativeBuildInputs = [
+    makeWrapper
+  ];
   installPhase = ''
     mkdir -p $out/bin $out/share/disko
     cp -r cli.nix default.nix types.nix disk-deactivate types $out/share/disko
@@ -11,6 +14,7 @@ stdenvNoCC.mkDerivation rec {
       -e "s|#!/usr/bin/env.*|#!/usr/bin/env bash|" \
       disko > $out/bin/disko
     chmod 755 $out/bin/disko
+    wrapProgram $out/bin/disko --prefix NIX_PATH : "nixpkgs=${path}"
   '';
   meta = with lib; {
     description = "Format disks with nix-config";
