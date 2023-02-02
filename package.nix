50adcfe3038ce749771832c3ab78e6d97451139c
Jörg Thalheim <joerg@thalheim.io>
2023-02-02 13:51:49 +0100

    disko: fix packaging missing nix files

--- package.nix
+++ package.nix
@@ -1,27 +1,15 @@
 { stdenvNoCC, lib }:
 
-let
-  inclFiles = {src, name}: files: lib.cleanSourceWith {
-    inherit src name;
-    filter = _path: _type: _type == "regular" && lib.any (file: builtins.baseNameOf _path == file) files;
-  };
-in
 stdenvNoCC.mkDerivation rec {
   name = "disko";
-  src = inclFiles { inherit name; src = ./.; } [
-    "disko"
-    "cli.nix"
-    "default.nix"
-    "types.nix"
-    "options.nix"
-  ];
+  src = ./.;
   installPhase = ''
     mkdir -p $out/bin $out/share/disko
-    cp -r $src/* $out/share/disko
+    cp -r cli.nix default.nix types.nix disk-deactivate types $out/share/disko
     sed \
       -e "s|libexec_dir=\".*\"|libexec_dir=\"$out/share/disko\"|" \
       -e "s|#!/usr/bin/env.*|#!/usr/bin/env bash|" \
-      $src/disko > $out/bin/disko
+      disko > $out/bin/disko
     chmod 755 $out/bin/disko
   '';
   meta = with lib; {
