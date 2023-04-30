8b43938cf77bbeddec127a5398b5dba3578c4290
lassulus <git@lassul.us>
2023-04-30 13:59:54 +0200

    luks-lvm: document secret.key newline

--- example/luks-lvm.nix
+++ example/luks-lvm.nix
@@ -30,6 +30,8 @@
                 type = "luks";
                 name = "crypted";
                 extraOpenArgs = [ "--allow-discards" ];
+                # if you want to use the key for interactive login be sure there is no trailing newline
+                # for example use `echo -n "password" > /tmp/secret.key`
                 keyFile = "/tmp/secret.key";
                 content = {
                   type = "lvm_pv";
