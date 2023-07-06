96cc49dbb8c586d08559e1bf84373bc7ee0b6b71
arch <arch@local>
2023-07-06 20:33:44 +0200

    add additionalKeyFiles option to luks

--- example/luks-lvm.nix
+++ example/luks-lvm.nix
@@ -33,6 +33,7 @@
                 # if you want to use the key for interactive login be sure there is no trailing newline
                 # for example use `echo -n "password" > /tmp/secret.key`
                 settings.keyFile = "/tmp/secret.key";
+                additionalKeyFiles = ["/tmp/additionalSecret.key"];
                 content = {
                   type = "lvm_pv";
                   vg = "pool";
