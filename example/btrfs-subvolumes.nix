f44564a7446455cd5fea4004f3a025b3baeb784a
rogarb <rogarb@rgarbage.fr>
2023-07-21 15:13:46 +0200

    fix(btrfs): update BTRFS example
    This takes into account the fix from commit 7f3b98f.

--- example/btrfs-subvolumes.nix
+++ example/btrfs-subvolumes.nix
@@ -24,18 +24,26 @@
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
                 subvolumes = {
                   # Subvolume name is different from mountpoint
                   "/rootfs" = {
                     mountpoint = "/";
                   };
-                  # Mountpoints inferred from subvolume name
+                  # Subvolume name is the same as the mountpoint
                   "/home" = {
                     mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
                   };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  "/home/user" = { };
+                  # Parent is not mounted so the mountpoint must be set
                   "/nix" = {
                     mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
                   };
+                  # This subvolume will be created but not mounted
                   "/test" = { };
                 };
               };
