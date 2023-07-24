ce52471dfdd9d5b79f9b604128ebacc30f6d7074
rogarb <rogarb@rgarbage.fr>
2023-07-24 19:35:24 +0200

    tests(btrfs): update to fit new behaviour

--- tests/btrfs-subvolumes.nix
+++ tests/btrfs-subvolumes.nix
@@ -6,7 +6,8 @@ makeDiskoTest {
   name = "btrfs-subvolumes";
   disko-config = ../example/btrfs-subvolumes.nix;
   extraTestScript = ''
-    machine.succeed("test -e /test");
+    machine.succeed("test ! -e /test");
+    machine.succeed("test -e /home/user");
     machine.succeed("btrfs subvolume list / | grep -qs 'path test$'");
     machine.succeed("btrfs subvolume list / | grep -qs 'path nix$'");
     machine.succeed("btrfs subvolume list / | grep -qs 'path home$'");
