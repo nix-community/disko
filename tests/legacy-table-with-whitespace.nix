338050a3cc4c01128094661161bfd0c00c7d483f
Chris Scutcher <chris@scutcher.uk>
2023-08-25 14:49:08 +0100

    tests: Add failing test to demonstrate issues with whitespace in part names
    Reproduces #130.
    
    For new style table the generated script has a few problems for example;
    
    ```sh
        sgdisk \
          --new=2:0:+100M \
          --change-name=2:disk-vdb-name with spaces \
          --typecode=2:EF00 \
          /dev/vdb
    ```
    
    and
    
    ```sh
    mkfs.vfat \
             \
            /dev/disk/by-partlabel/disk-vdb-name with spaces
    ```
    
    Legacy table style generates slightly different problems e.g.;
    
    ```sh
    parted -s /dev/vdb -- mkpart name with spaces  1MiB 100MiB
    ```

---
+++ tests/legacy-table-with-whitespace.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { }
+, diskoLib ? pkgs.callPackage ../lib { }
+}:
+diskoLib.testLib.makeDiskoTest {
+  inherit pkgs;
+  name = "legacy-table-with-whitespace";
+  disko-config = ../example/legacy-table-with-whitespace.nix;
+  extraTestScript = ''
+    machine.succeed("mountpoint /");
+    machine.succeed("mountpoint /name_with_spaces");
+  '';
+}
