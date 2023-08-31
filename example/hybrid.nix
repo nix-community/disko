b55fc2ea85257a3dcd0a517ea94d0be40130a97f
Valentin Gagarin <valentin@fricklerhandwerk.de>
2023-08-31 23:51:05 +0200

    make hybrid example more generic
    otherwise it requires editing the file before actually using it for the quick start guide

--- example/hybrid.nix
+++ example/hybrid.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-Samsung_SSD_850_EVO_250GB_S21PNXAGB12345";
+        device = "/dev/nvme0n1"; # you may want to replace this by `/dev/disk/by-id/<UUID>`
         content = {
           type = "gpt";
           partitions = {
