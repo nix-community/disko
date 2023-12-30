82b58161dbd6145d0409630b12ab67f7104c3003
Michael Hoang <enzime@users.noreply.github.com>
2023-12-30 11:23:57 +1100

    tests: check `canmount` ZFS flag is respected

--- tests/non-root-zfs.nix
+++ tests/non-root-zfs.nix
@@ -14,13 +14,35 @@ diskoLib.testLib.makeDiskoTest {
     filesystem = machine.execute("stat --file-system --format=%T /mnt/storage")[1].rstrip()
     print(f"/mnt/storage {filesystem=}")
     assert filesystem == "zfs", "/mnt/storage is not ZFS"
+
+    machine.fail("mountpoint /mnt/storage2")
+    machine.succeed("mountpoint /mnt/storage2/dataset")
+
+    filesystem = machine.execute("stat --file-system --format=%T /mnt/storage2")[1].rstrip()
+    print(f"/mnt/storage2 {filesystem=}")
+    assert filesystem != "zfs", "/mnt/storage should not be ZFS"
+
+    filesystem = machine.execute("stat --file-system --format=%T /mnt/storage2/dataset")[1].rstrip()
+    print(f"/mnt/storage2/dataset {filesystem=}")
+    assert filesystem == "zfs", "/mnt/storage/dataset is not ZFS"
   '';
   extraTestScript = ''
     machine.succeed("mountpoint /storage")
     machine.succeed("mountpoint /storage/dataset")
 
     filesystem = machine.execute("stat --file-system --format=%T /storage")[1].rstrip()
-    print(f"/mnt/storage {filesystem=}")
+    print(f"/storage {filesystem=}")
     assert filesystem == "zfs", "/storage is not ZFS"
+
+    machine.fail("mountpoint /storage2")
+    machine.succeed("mountpoint /storage2/dataset")
+
+    filesystem = machine.execute("stat --file-system --format=%T /storage2")[1].rstrip()
+    print(f"/storage2 {filesystem=}")
+    assert filesystem != "zfs", "/storage should not be ZFS"
+
+    filesystem = machine.execute("stat --file-system --format=%T /storage2/dataset")[1].rstrip()
+    print(f"/storage2/dataset {filesystem=}")
+    assert filesystem == "zfs", "/storage/dataset is not ZFS"
   '';
 }
