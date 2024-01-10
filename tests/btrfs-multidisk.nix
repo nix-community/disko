5331a3e005a0af3e5006bc0254977c69d169d37b
stephen andary <stephen@jamminmusic.com>
2024-01-10 14:24:59 -0500

    add multidisk test

---
+++ tests/btrfs-multidisk.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> {},
+  diskoLib ? pkgs.callPackage ../lib {},
+}:
+diskoLib.testLib.makeDiskoTest {
+  inherit pkgs;
+  name = "btrfs-multidisk";
+  disko-config = ../example/btrfs-multidisk.nix;
+  extraTestScript = ''
+    machine.succeed("test ! -e /test");
+    machine.succeed("test -e /home/user");
+    machine.succeed("btrfs subvolume list / | grep -qs 'path test$'");
+    machine.succeed("btrfs subvolume list / | grep -qs 'path nix$'");
+    machine.succeed("btrfs subvolume list / | grep -qs 'path home$'");
+  '';
+}
