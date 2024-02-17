c13d481b78d81ee1925fc5ecfad2d7385b965b78
lassulus <git@lassul.us>
2024-02-17 05:02:53 +0100

    hybrid-mbr: cleanup test, wait for udev after paritioning

--- example/hybrid-mbr.nix
+++ example/hybrid-mbr.nix
@@ -1,9 +1,9 @@
-{disks ? ["/dev/sda"], ...}: {
+{
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = builtins.elemAt disks 0;
+        device = "/dev/sdb";
         content = {
           type = "gpt";
           efiGptPartitionFirst = false;
