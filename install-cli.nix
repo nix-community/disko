bde7dd352c07d43bd5b8245e6c39074a391fdd46
Jörg Thalheim <joerg@thalheim.io>
2024-02-27 20:41:22 +0100

    add disko-install command
    disko-install combines disko and nixos-install into a single command.
    
    Example usage:
    
      sudo ./disko-install --flake /someflake#eva --disk sda /dev/zvol/zroot/test

---
+++ install-cli.nix
@@ -0,0 +1,43 @@
+{ flake
+, flakeAttr
+, diskMappings
+, canTouchEfiVariables ? false
+, rootMountPoint ? "/mnt"
+}:
+let
+  originalSystem = (builtins.getFlake "${flake}").nixosConfigurations."${flakeAttr}";
+  diskoSystem =
+    let
+      lib = originalSystem.lib;
+
+      modifiedDisks = builtins.mapAttrs
+        (name: value: let
+          dev = if  diskMappings ? ${name} then
+            diskMappings.${name}
+          else
+            throw "No device passed for disk '${name}'";
+        in value // {
+          device = dev;
+          content = value.content // { device = dev; };
+        })
+        originalSystem.config.disko.devices.disk;
+
+      cleanedDisks = lib.filterAttrsRecursive (n: _: !lib.hasPrefix "_" n) modifiedDisks;
+    in
+    originalSystem.extendModules {
+      modules = [{
+        disko.rootMountPoint = rootMountPoint;
+        disko.devices.disk = lib.mkVMOverride cleanedDisks;
+      }];
+    };
+   installSystem = originalSystem.extendModules {
+     modules = [({ lib, ... }: {
+       boot.loader.efi.canTouchEfiVariables = lib.mkVMOverride canTouchEfiVariables;
+       boot.loader.grub.devices = lib.mkVMOverride diskoSystem.config.boot.loader.grub.devices;
+     })];
+   };
+in
+{
+  installToplevel = installSystem.config.system.build.toplevel;
+  inherit (diskoSystem.config.system.build) formatScript mountScript diskoScript;
+}
---
+++ tests/disko-install/default.nix
@@ -0,0 +1,45 @@
+{ pkgs ? import <nixpkgs> { }, self }:
+let
+  disko-install = pkgs.callPackage ../../disko-install.nix { };
+  toplevel = self.nixosConfigurations.testmachine.config.system.build.toplevel;
+
+  dependencies = [
+    pkgs.stdenv.drvPath
+    toplevel
+    self.nixosConfigurations.testmachine.config.system.build.diskoScript
+  ] ++ builtins.map (i: i.outPath) (builtins.attrValues self.inputs);
+
+  closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
+in
+pkgs.nixosTest {
+  name = "disko-test";
+  nodes.machine = {
+    virtualisation.emptyDiskImages = [ 4096 ];
+    virtualisation.memorySize = 3000;
+    environment.etc."install-closure".source = "${closureInfo}/store-paths";
+  };
+
+  testScript = ''
+    def create_test_machine(oldmachine=None, args={}): # taken from <nixpkgs/nixos/tests/installer.nix>
+        machine = create_machine({
+          "qemuFlags":
+            '-cpu max -m 1024 -virtfs local,path=/nix/store,security_model=none,mount_tag=nix-store,'
+            f' -drive file={oldmachine.state_dir}/empty0.qcow2,id=drive1,if=none,index=1,werror=report'
+            f' -device virtio-blk-pci,drive=drive1',
+        } | args)
+        driver.machines.append(machine)
+        return machine
+    machine.succeed("lsblk >&2")
+
+    print(machine.succeed("tty"))
+    machine.succeed("${disko-install}/bin/disko-install --disk main /dev/vdb --flake ${../..}#testmachine")
+    # test idempotency
+    machine.succeed("${disko-install}/bin/disko-install --mode mount --disk main /dev/vdb --flake ${../..}#testmachine")
+    machine.shutdown()
+
+    new_machine = create_test_machine(oldmachine=machine, args={ "name": "after_install" })
+    new_machine.start()
+    name = new_machine.succeed("hostname").strip()
+    assert name == "disko-machine", f"expected hostname 'disko-machine', got {name}"
+  '';
+}
