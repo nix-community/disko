078c23ed6f684353349d73f7265c178ff26103f4
Jörg Thalheim <joerg@thalheim.io>
2024-03-04 15:43:23 +0100

    bcachefs: drop zfs hack

--- tests/bcachefs.nix
+++ tests/bcachefs.nix
@@ -12,13 +12,5 @@ diskoLib.testLib.makeDiskoTest {
   # so that the installer boots with a bcachefs enabled kernel
   extraInstallerConfig = {
     boot.supportedFilesystems = [ "bcachefs" ];
-    # disable zfs so we can support latest kernel
-    nixpkgs.overlays = [
-      (_final: super: {
-        zfs = super.zfs.overrideAttrs (_: {
-          meta.platforms = [ ];
-        });
-      })
-    ];
   };
 }
