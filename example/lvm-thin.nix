de29130286bbad9e17ea71f6bd99a272a1ec5c75
Matthew_Cash <matthew@matthew-cash.com>
2024-06-24 23:29:25 -0700

    lvm_vg: add lvm thinpool/thinlv support
    Co-authored-by: ajs124 <git@ajs124.de>

---
+++ example/lvm-thin.nix
@@ -0,0 +1,71 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = "/dev/vdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            primary = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "mainpool";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      mainpool = {
+        type = "lvm_vg";
+        lvs = {
+          thinpool = {
+            size = "100M";
+            lvm_type = "thin-pool";
+          };
+          root = {
+            size = "10M";
+            lvm_type = "thinlv";
+            pool = "thinpool";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          home = {
+            size = "10M";
+            lvm_type = "thinlv";
+            pool = "thinpool";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+          raw = {
+            size = "10M";
+          };
+        };
+      };
+    };
+  };
+}
