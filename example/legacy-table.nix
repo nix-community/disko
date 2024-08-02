2b0e2d6d90be4c4b85c0c059b767f05ee850d9e9
goyalyashpal <yashpalgoyal1304@gmail.com>
2024-08-02 16:50:04 +0000

    refac(examples): Denest the source

--- example/legacy-table.nix
+++ example/legacy-table.nix
@@ -1,40 +1,38 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "1M";
-              end = "500M";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            }
-            {
-              name = "root";
-              start = "500M";
-              end = "100%";
-              part-type = "primary";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            }
-          ];
-        };
-      };
-    };
+  disko.devices.disk.main = {
+    device = "/dev/sda";
+    type = "disk";
   };
+
+  disko.devices.disk.main.content = {
+    type = "table";
+    format = "gpt";
+  };
+
+  disko.devices.disk.main.content.partitions = [
+    {
+      name = "ESP";
+      start = "1M";
+      end = "500M";
+      bootable = true;
+      content = {
+        type = "filesystem";
+        format = "vfat";
+        mountpoint = "/boot";
+      };
+    }
+    {
+      name = "root";
+      start = "500M";
+      end = "100%";
+      part-type = "primary";
+      bootable = true;
+      content = {
+        type = "filesystem";
+        format = "ext4";
+        mountpoint = "/";
+      };
+    }
+  ];
 }
 
