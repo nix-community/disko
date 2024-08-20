cc86fe1a7c5171f1949cc76b98dbd990cfb70b0c
matthewcroughan <matt@croughan.sh>
2024-08-20 10:21:58 +0100

    f2fs: init

---
+++ example/f2fs.nix
@@ -0,0 +1,34 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-path/pci-0000:02:00.0-nvme-1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              end = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              name = "root";
+              end = "-0";
+              content = {
+                type = "filesystem";
+                format = "f2fs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
