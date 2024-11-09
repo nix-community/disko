e8df0397e757a46eff4f48f711079c4d38c6e21a
Sk7Str1pe <159684143+Sk7Str1pe@users.noreply.github.com>
2024-11-09 14:29:44 +0300

    Create luks-btrfs-tpm.nix

---
+++ example/luks-btrfs-tpm.nix
@@ -0,0 +1,46 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                };
+                tpmEnroll = true;
+                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ];            
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
