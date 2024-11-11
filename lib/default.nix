nix-community/disko

commit 94bc0f5bb0dff8b98597f1eb66a7156469932221

mode destroy: Add confirmation dialogue
The new confirmation dialogue is only shown for the new outputs
introduced in the previous commits. The existing outputs do not change
behavior to keep backwards compatibility.

Fixes #725

lib/default.nix
@@ -462,7 +462,7 @@ let
                 '';
                 destroyFormatMount = (diskoLib.writeCheckedBash { inherit pkgs checked; }) "/bin/disko-destroy-format-mount" ''
                   export PATH=${lib.makeBinPath ((cfg.config._packages pkgs) ++ [ pkgs.bash ] ++ destroyDependencies)}:$PATH
-                  ${cfg.config._disko}
+                  ${cfg.config._destroyFormatMount}
                 '';
 
                 # These are useful to skip copying executables uploading a script to an in-memory installer
@@ -479,7 +479,7 @@ let
                   ${cfg.config._formatMount}
                 '';
                 destroyFormatMountNoDeps = (diskoLib.writeCheckedBash { inherit pkgs checked; noDeps = true; }) "/bin/disko-destroy-format-mount" ''
-                  ${cfg.config._disko}
+                  ${cfg.config._destroyFormatMount}
                 '';
 
 
@@ -490,7 +490,7 @@ let
                 # `user.users.<name>.packages` and `home.packages`, see https://github.com/nix-community/disko/issues/454
                 destroyScript = (diskoLib.writeCheckedBash { inherit pkgs checked; }) "disko-destroy" ''
                   export PATH=${lib.makeBinPath destroyDependencies}:$PATH
-                  ${cfg.config._destroy}
+                  ${cfg.config._legacyDestroy}
                 '';
 
                 formatScript = (diskoLib.writeCheckedBash { inherit pkgs checked; }) "disko-format" ''
@@ -510,7 +510,7 @@ let
 
                 # These are useful to skip copying executables uploading a script to an in-memory installer
                 destroyScriptNoDeps = (diskoLib.writeCheckedBash { inherit pkgs checked; noDeps = true; }) "disko-destroy" ''
-                  ${cfg.config._destroy}
+                  ${cfg.config._legacyDestroy}
                 '';
 
                 formatScriptNoDeps = (diskoLib.writeCheckedBash { inherit pkgs checked; noDeps = true; }) "disko-format" ''
@@ -526,11 +526,12 @@ let
                 '';
               };
           };
-          _destroy = lib.mkOption {
+          _legacyDestroy = lib.mkOption {
             internal = true;
             type = lib.types.str;
             description = ''
               The script to unmount (& destroy) all devices defined by disko.devices
+              Does not ask for confirmation! Depracated in favor of _destroy
             '';
             default = ''
               umount -Rv "${rootMountPoint}" || :
@@ -540,6 +541,44 @@ let
                 $BASH ${../disk-deactivate}/disk-deactivate "$dev"
               done
             '';
+          };
+          _destroy = lib.mkOption {
+            internal = true;
+            type = lib.types.str;
+            description = ''
+              The script to unmount (& destroy) all devices defined by disko.devices
+            '';
+            default =
+              let
+                selectedDisks = lib.escapeShellArgs (lib.catAttrs "device" (lib.attrValues devices.disk));
+              in
+              ''
+                if [ "$1" != "--yes-wipe-all-disks" ]; then
+                  echo "WARNING: This will destroy all data on the disks defined in disko.devices, which are:"
+                  echo
+                  # shellcheck disable=SC2043
+                  for dev in ${selectedDisks}; do
+                    echo "  - $dev"
+                  done
+                  echo
+                  echo "    (If you want to skip this dialogue, pass --yes-wipe-all-disks)"
+                  echo
+                  echo "Are you sure you want to wipe the devices listed above?"
+                  read -rp "Type 'yes' to continue, anything else to abort: " confirmation
+
+                  if [ "$confirmation" != "yes" ]; then
+                    echo "Aborted."
+                    exit 1
+                  fi
+                fi
+
+                umount -Rv "${rootMountPoint}" || :
+
+                # shellcheck disable=SC2043
+                for dev in ${selectedDisks}; do
+                  $BASH ${../disk-deactivate}/disk-deactivate "$dev"
+                done
+              '';
           };
           _create = lib.mkOption {
             internal = true;
@@ -586,6 +625,19 @@ let
             type = lib.types.str;
             description = ''
               The script to umount, create and mount all devices defined by disko.devices
+              Deprecated in favor of _destroyFormatMount
+            '';
+            default = ''
+              ${cfg.config._legacyDestroy}
+              ${cfg.config._create}
+              ${cfg.config._mount}
+            '';
+          };
+          _destroyFormatMount = lib.mkOption {
+            internal = true;
+            type = lib.types.str;
+            description = ''
+              The script to unmount, create and mount all devices defined by disko.devices
             '';
             default = ''
               ${cfg.config._destroy}

disko
@@ -9,6 +9,7 @@ declare disko_config
 # mount was chosen as the default mode because it's less destructive
 mode=mount
 nix_args=()
+skip_destroy_safety_check=false
 
 # DISKO_VERSION is set by the wrapper in package.nix
 DISKO_VERSION="${DISKO_VERSION:="unknown! This is a bug, please report it!"}"
@@ -44,6 +45,8 @@ Options:
 * --no-deps
   avoid adding another dependency closure to an in-memory installer
     requires all necessary dependencies to be available in the environment
+* --yes-wipe-all-disks
+  skip the safety check for destroying partitions, useful for automation
 * --debug
   run with set -x
 * --help
@@ -94,6 +97,9 @@ while [[ $# -gt 0 ]]; do
       ;;
     --no-deps)
       nix_args+=(--arg noDeps true)
+      ;;
+    --yes-wipe-all-disks)
+      skip_destroy_safety_check=true
       ;;
     --show-trace)
       nix_args+=("$1")
@@ -169,8 +175,14 @@ script=$(nixBuild "${libexec_dir}"/cli.nix \
   --argstr mode "$mode" \
   "${nix_args[@]}"
 )
+
+command=("$(echo "$script"/bin/*)")
+if [[ $mode = "destroy,format,mount" && $skip_destroy_safety_check = true ]]; then
+  command+=("--yes-wipe-all-disks")
+fi
+
 if [[ -n "${dry_run+x}" ]]; then
-  echo "$script"
+  echo "${command[@]}"
 else
-  exec "$script"
+  exec "${command[@]}"
 fi

lib/make-disk-image.nix
@@ -84,7 +84,7 @@ let
     ${lib.optionalString diskoCfg.testMode ''
       export IN_DISKO_TEST=1
     ''}
-    ${lib.getExe systemToInstall.config.system.build.destroyFormatMount}
+    ${lib.getExe systemToInstall.config.system.build.destroyFormatMount} --yes-wipe-all-disks
   '';
 
   installer = lib.optionalString cfg.copyNixStore ''

lib/tests.nix
@@ -263,7 +263,7 @@ let
             machine.succeed("${lib.getExe tsp-format}")
             machine.succeed("${lib.getExe tsp-mount}")
             machine.succeed("${lib.getExe tsp-mount}") # verify that mount is idempotent
-            machine.succeed("${lib.getExe tsp-disko}") # verify that we can destroy and recreate
+            machine.succeed("${lib.getExe tsp-disko} --yes-wipe-all-disks") # verify that we can destroy and recreate
             machine.succeed("mkdir -p /mnt/home")
             machine.succeed("touch /mnt/home/testfile")
             machine.succeed("${lib.getExe tsp-format}") # verify that format is idempotent
@@ -274,7 +274,7 @@ let
             machine.succeed("${lib.getExe nodes.machine.system.build.format}")
             machine.succeed("${lib.getExe nodes.machine.system.build.mount}")
             machine.succeed("${lib.getExe nodes.machine.system.build.mount}") # verify that mount is idempotent
-            machine.succeed("${lib.getExe nodes.machine.system.build.destroyFormatMount}") # verify that we can destroy and recreate again
+            machine.succeed("${lib.getExe nodes.machine.system.build.destroyFormatMount} --yes-wipe-all-disks") # verify that we can destroy and recreate again
             machine.succeed("mkdir -p /mnt/home")
             machine.succeed("touch /mnt/home/testfile")
             machine.succeed("${lib.getExe nodes.machine.system.build.format}") # verify that format is idempotent

commit 0f53e8c4b388fd5a994d6c53c4d97a54227a39f3

destroy: Fix shellcheck warning in nixos 24.05
Regression introduced in 94bc0f5bb0dff8b98597f1eb66a7156469932221,
because I developed against unstable, which has
https://github.com/NixOS/nixpkgs/pull/333744 merged, while 24.05
doesn't.

Partly shellchecks mistake, see
https://github.com/koalaman/shellcheck/issues/2891 but whatever, we
can't do much more about it than ignore the warning.

Fixes #868

lib/default.nix
@@ -556,7 +556,7 @@ let
                 if [ "$1" != "--yes-wipe-all-disks" ]; then
                   echo "WARNING: This will destroy all data on the disks defined in disko.devices, which are:"
                   echo
-                  # shellcheck disable=SC2043
+                  # shellcheck disable=SC2043,2041
                   for dev in ${selectedDisks}; do
                     echo "  - $dev"
                   done