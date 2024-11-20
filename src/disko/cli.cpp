58c3e090d0321ac8cb62821527f5f9b0a71d7bd7
Sk7Str1pe <finenkofedor@gmail.com>
2024-11-20 22:05:06 +0300

    colorized output

--- src/disko/cli.cpp
+++ src/disko/cli.cpp
@@ -4,29 +4,63 @@
 #include <getopt.h>
 #include <iostream>
 
+// define colors for beautiful (xd) output
+#define RESET "\033[0m"
+#define BLACK "\033[30m"              /* Black */
+#define RED "\033[31m"                /* Red */
+#define GREEN "\033[32m"              /* Green */
+#define YELLOW "\033[33m"             /* Yellow */
+#define BLUE "\033[34m"               /* Blue */
+#define MAGENTA "\033[35m"            /* Magenta */
+#define CYAN "\033[36m"               /* Cyan */
+#define WHITE "\033[37m"              /* White */
+#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
+#define BOLDRED "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
+
 /*help message*/
 void showUsage() {
-  std::cerr
-      << "Disko - tool for declarative disk partitioning.\n"
-      << "Usage: " << "disko2 " << "[mode] " << "<args> disko-config.nix \n\n"
-      << "Modes:\n"
-      << "    wipe       ->   wipe disk\n"
-      << "    format     ->   create partition table and filesystems\n"
-      << "    mount      ->   mount disks\n"
-      << "    init       ->   initiate disks, run wipe,format,mount in "
-         "sequence\n"
-      << "    generate   ->   generate configuration from current disks "
-         "configuration\n\n"
-      << "Args:\n"
-      << "    -f/--flake <path/url>    ->   define flake location\n"
-      << "    -c/--config <file.nix>   ->   define config file\n"
-      << "    -m/--mount <path>        ->   sets path where disks should be "
-         "mounted (default: /mnt)\n"
-      << "    --arg 'name value'       ->   arguments for nix-build\n"
-      << "    -d/--dry-run             ->   show what script WOULD do\n"
-      << "    --yes-wipe-everything    ->   skip safety check\n"
-      << "    -v/--verbose             ->   increase verbosity of output\n"
-      << "    -h/--help                ->   show this message" << std::endl;
+  std::cerr << BOLDMAGENTA "Disko" RESET " - tool for" BOLDBLUE
+                           " declarative " RESET "disk partitioning.\n"
+            << BOLDYELLOW "Usage: " RESET "disko2 " RED "[mode] " CYAN "<args> "
+            << BLUE "-c disko-config.nix" RESET ", or\n"
+            << "       disko2 " RED "[mode] " CYAN "<args> " BLUE
+               "--flake github:foo/bar#disk-config\n\n"
+            << BOLDRED "Modes:\n"
+            << BLUE "    wipe  " RESET "     ->   " CYAN "wipe disk\n"
+            << BLUE "    format  " RESET "   ->   " CYAN
+                    "create partition table and filesystems\n"
+            << BLUE "    mount      " RESET "->  " CYAN " mount disks\n"
+            << BLUE "    init      " RESET " ->  " CYAN
+                    " initiate disks, run " RESET "wipe,format,mount " CYAN
+                    "in sequence\n"
+            << BLUE "    generate   " RESET "-> " CYAN
+                    "  generate config file from current disks "
+                    "state\n\n"
+            << BOLDCYAN "Args:\n"
+            << YELLOW "    -f/--flake <path/url> " RESET "   ->  " GREEN
+                      " define flake location\n"
+            << YELLOW "    -c/--config <file.nix> " RESET "  -> " GREEN
+                      "  define config file\n"
+            << YELLOW "    -m/--mount <path>   " RESET "     ->  " GREEN
+                      " sets path where disks should be "
+                      "mounted (default: /mnt)\n"
+            << YELLOW "    --arg 'name value'   " RESET "    ->  " GREEN
+                      " arguments for nix-build\n"
+            << YELLOW "    -d/--dry-run            " RESET " ->  " GREEN
+                      " show what script WOULD do\n"
+            << YELLOW "    --yes-wipe-everything   " RESET " ->  " GREEN
+                      " skip safety check\n"
+            << YELLOW "    -v/--verbose           " RESET "  ->  " GREEN
+                      " increase verbosity of output\n"
+            << YELLOW "    -h/--help          " RESET "      -> " GREEN
+                      "  show this message"
+            << std::endl;
 
   /* --arg name value
      pass value to nix-build. can be used to set disk-names for example
