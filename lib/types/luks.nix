cbc1f418d0cd79bf84aae3490ba8bb6222d8e81d
Philipp Schmitt <philipp@schmitt.co>
2025-04-13 10:59:04 +0200

    Improve luks device detection

--- lib/types/luks.nix
+++ lib/types/luks.nix
@@ -130,7 +130,7 @@ in
     _create = diskoLib.mkCreateOption {
       inherit config options;
       default = ''
-        if ! blkid "${config.device}" >/dev/null || ! (blkid "${config.device}" -o export | grep -q '^TYPE='); then
+        if ! blkid "${config.device}" >/dev/null || ! (cryptsetup isLuks "${config.device}"); then
           ${lib.optionalString config.askPassword ''
             promptSecret() {
               prompt=$1
@@ -158,13 +158,15 @@ in
             done
           ''}
           cryptsetup -q luksFormat "${config.device}" ${toString config.extraFormatArgs} ${keyFileArgs}
-          ${cryptsetupOpen} --persistent
-          ${toString (
-            lib.forEach config.additionalKeyFiles (keyFile: ''
-              cryptsetup luksAddKey "${config.device}" ${keyFile} ${keyFileArgs}
-            '')
-          )}
         fi
+
+        ${cryptsetupOpen} --persistent
+        ${toString (
+          lib.forEach config.additionalKeyFiles (keyFile: ''
+            cryptsetup luksAddKey "${config.device}" ${keyFile} ${keyFileArgs}
+          '')
+        )}
+
         ${lib.optionalString (config.content != null) config.content._create}
       '';
     };
