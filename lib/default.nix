6b4988942222bac5d392a60841b57aeea299d55a
Philipp Schmitt <philipp@schmitt.co>
2025-04-13 16:28:47 +0200

    Fix _destroy

--- lib/default.nix
+++ lib/default.nix
@@ -868,9 +868,7 @@ let
               # shellcheck disable=SC2043,2041
               for dev in ${
                 toString (
-                  lib.catAttrs "device" (
-                    lib.attrValues (lib.filterAttrs (name: disk: (disk.destroy or true)) devices.disk)
-                  )
+                  lib.catAttrs "device" (lib.attrValues (lib.filterAttrs (name: disk: disk.destroy) devices.disk))
                 )
               };
               do
@@ -887,9 +885,8 @@ let
             '';
             default =
               let
-                selectedDisks = lib.escapeShellArgs (
-                  lib.catAttrs "device" (lib.filterAttrs (name: disk: (disk.destroy or true)) devices.disk)
-                );
+                disksToWipe = lib.attrValues (lib.filterAttrs (name: disk: disk.destroy) devices.disk);
+                selectedDisks = lib.escapeShellArgs (lib.catAttrs "device" disksToWipe);
               in
               ''
                 if [ "$1" != "--yes-wipe-all-disks" ]; then
@@ -914,11 +911,7 @@ let
                 umount -Rv "${rootMountPoint}" || :
 
                 # shellcheck disable=SC2043,2041
-                for dev in ${
-                  toString (
-                    lib.catAttrs "device" (lib.filterAttrs (name: disk: (disk.destroy or true)) devices.disk)
-                  )
-                }; do
+                for dev in ${toString (lib.catAttrs "device" disksToWipe)}; do
                   $BASH ${../disk-deactivate}/disk-deactivate "$dev"
                 done
               '';
