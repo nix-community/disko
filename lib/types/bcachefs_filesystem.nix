cd7a2bf5a9485e07ebc31a0a173d5a717a2e579e
Jul Lang <jullanggit@proton.me>
2026-01-10 13:52:30 +0100

    fix accidental escape

--- lib/types/bcachefs_filesystem.nix
+++ lib/types/bcachefs_filesystem.nix
@@ -10,7 +10,7 @@
 let
   # colon separated list of devices, as UUID expansion is unreliable
   colonSeparatedDevices = ''
-    $(cat "${disko_devices_dir}/bcachefs-${config.name}" | paste -sd ":" -)
+    $(cat "$disko_devices_dir/bcachefs-${config.name}" | paste -sd ":" -)
   '';
 in
 {
