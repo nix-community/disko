88c9db00afd54dfa4db5193e04d82f51b0d7a1e4
yunfachi <git@ynf.sh>
2026-09-21 09:19:23 +0300

    lib/make-disk-image: use kernelModules arg

--- lib/make-disk-image.nix
+++ lib/make-disk-image.nix
@@ -41,10 +41,9 @@ let
       ]
       ++ (lib.optional configSupportsZfs "zfs")
       ++ cfg.extraRootModules;
-      kernel = pkgs.aggregateModules (
-        [
-          cfg.kernelPackages.kernel
-        ]
+      kernel = cfg.kernelPackages.kernel;
+      kernelModules = pkgs.aggregateModules (
+        [ cfg.kernelPackages.kernel ]
         ++ lib.optional (cfg.kernelPackages.kernel ? modules) cfg.kernelPackages.kernel.modules
         ++ lib.optional (
           lib.elem "zfs" cfg.extraRootModules || configSupportsZfs
